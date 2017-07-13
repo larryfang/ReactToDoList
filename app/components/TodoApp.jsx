dc98fac6da7e0352ddabb7227ceced5d5692e19e
larry fang <larry.l.fang@gmail.com>
2017-07-13 16:04:37 +1000

    Beginning Search ToDo Component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,13 +1,52 @@
 var React = require('react');
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+var ToDoSearch = require('SearchToDo');
 
 var TodoApp = React.createClass({
-  render: function () {
-    return (
-      <div>
-        TodoApp.jsx
-      </div>
-    )
-  }
+    getInitialState: function () {
+        return {
+            showCompleted: false,
+            searchText: "",
+            todos: [
+                {
+                    id: 1,
+                    text: 'Jogging'
+                }, {
+                    id: 2,
+                    text: 'Swimming'
+                }, {
+                    id: 3,
+                    text: 'Homework'
+                }, {
+                    id: 4,
+                    text: 'Play video games'
+                }
+            ]
+        };
+    },
+    handleAddTodo: function (text) {
+        alert('new todo: ' + text);
+    },
+
+    handleSearch: function (showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        })
+    },
+
+    render: function () {
+        var {todos} = this.state;
+
+        return (
+            <div>
+                <ToDoSearch onSearch={this.handleSearch}/>
+                <TodoList todos={todos}/>
+                <AddTodo onAddTodo={this.handleAddTodo}/>
+            </div>
+        )
+    }
 });
 
 module.exports = TodoApp;
